33bb5997f7fa2f3d0af5fbae90230ca5ce4c84a9
Bas <doppleb@gmail.com>
2020-01-28 15:40:01 +0100

    2020-01-28 Async verification for email is working through "existsByEmail" in User Repository. Returns a boolean which is used to check availability

--- src/main/java/nl/youngcapital/repository/UserRepository.java
+++ src/main/java/nl/youngcapital/repository/UserRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends CrudRepository<User, Long> {
 
     Iterable<User> findByName(String name);
+    boolean existsByEmail(String email);
 
 }
